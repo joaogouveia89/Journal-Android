80f64b9c57cddde930749259f092454c8d83f0ce
Joao Gouveia <goj2lis@lis-c-000wk.home>
2023-02-28 22:10:43 +0000

    very first version of the today journal screen, with repeated code and unstyled elements

--- app/src/main/java/com/example/journal/ui/todayjournal/TodayJournalFragment.kt
+++ app/src/main/java/com/example/journal/ui/todayjournal/TodayJournalFragment.kt
@@ -28,10 +28,10 @@ class TodayJournalFragment : Fragment() {
         _binding = FragmentTodayjournalBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textDashboard
+        /*val textView: TextView = binding.textDashboard
         dashboardViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
-        }
+        }*/
         return root
     }
 
